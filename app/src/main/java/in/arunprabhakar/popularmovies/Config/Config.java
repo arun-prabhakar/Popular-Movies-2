568c3547d2e2d900c8ab4741d418dceabaf7d6db
Arun Prabhakar <arunprabhakar.t@gmail.com>
2017-05-26 03:22:47 +0530

    add files

---
+++ app/src/main/java/in/arunprabhakar/popularmovies/Config/Config.java
@@ -0,0 +1,9 @@
+package in.arunprabhakar.popularmovies.Config;
+
+/**
+ * Configuration file for app needed info. Currently just the API Key
+ */
+
+public class Config {
+    public static final String API_KEY = "379c9f5b3bb5b595a298c5b3340c59fc";
+}
